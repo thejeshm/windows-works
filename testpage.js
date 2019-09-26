af6ce508582b71a33a0dcc75a1f684c935dd4b96
Thejesh Mannar (HCL America Inc) <v-thmann@microsoft.com>
2019-09-27 00:06:02 +0530

    added the output file

--- testpage.js
+++ testpage.js
@@ -35,7 +35,7 @@ $(function() {
     return false;
   });
 });
-
+//output files are here.......
 $.fn.slideFadeToggle = function(easing, callback) {
   return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
 };*/
